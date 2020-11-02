0b34fcaf901e59a20c273e14c17499709635c2fa
mickac <kaczynski.mp@gmail.com>
2020-11-02 23:53:19 +0100

    Stylistic and logic changes for add book form

--- bookManagement/forms.py
+++ bookManagement/forms.py
@@ -2,8 +2,21 @@ from django import forms
 
 from .models import Book
 
+ISBN_TYPE_CHOICES = [
+    ('10', 'ISBN 10'),
+    ('13', 'ISBN 13'),
+]
 class BookForm(forms.ModelForm):
 
     class Meta:
         model = Book
-        fields = ('title', 'authors', 'publishedDate', 'isbnType', 'isbnId', 'pageCount', 'image', 'language')
+        fields = '__all__'
+        labels = {
+            'title': 'Book title',
+            'authors': 'Author(s)',
+            'publishedDate': 'Date of publish',
+            'isbnType': 'ISBN Type',
+            'isbnId': 'ISBN ID',
+            'pageCount': 'Number of pages',
+            'image': 'URL of book cover',
+        }
--- bookManagement/models.py
+++ bookManagement/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 
+ISBN_TYPE_CHOICES = [
+    ('10', 'ISBN 10'),
+    ('13', 'ISBN 13'),
+]
+
 class Book(models.Model):
     title = models.CharField(max_length = 50)
     authors = models.CharField(max_length = 50)
     publishedDate = models.DateField()
-    isbnType = models.CharField(max_length = 7)
+    isbnType = models.CharField(max_length = 7, choices=ISBN_TYPE_CHOICES)
     isbnId = models.CharField(max_length = 13)
     pageCount = models.PositiveIntegerField()
     image = models.URLField()
