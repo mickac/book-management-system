fe139e6309d6d451202d7e4ab2c20732b959441b
mickac <kaczynski.mp@gmail.com>
2020-11-03 17:04:36 +0100

    Adding search funcionality and preparing search tooltip to inform users about date format they have to use

--- bookManagement/urls.py
+++ bookManagement/urls.py
@@ -6,5 +6,6 @@ appname = 'bookManagement'
 urlpatterns = [
     path('', views.index, name='index'),
     path('book_add', views.book_add, name='book_add'),
-    path('book_list', views.book_list, name='book_list')
+    path('book_list', views.book_list, name='book_list'),
+    path('book_search', views.book_search, name='book_search')
 ]
--- bookManagement/views.py
+++ bookManagement/views.py
@@ -65,10 +65,33 @@ def book_list(request):
 
 def book_search(request):
         try:
-            query = request.GET.get('q')
-            book_list = Book.objects.filter(
-                Q(title__icontains=query) | Q(authors__icontains=query) | Q(language__icontains=query) | Q(publishedDate__icontains=query)
-            )
+            # 2020-10-20 to 2020-10-30
+            keyword = request.POST.get('keyword')
+            parameter = request.POST.get('parameter')
+            if (parameter == 'title'):
+                book_list = Book.objects.filter(
+                    Q(title__icontains=keyword)
+                )
+            elif (parameter == 'authors'):
+                book_list = Book.objects.filter(
+                    Q(authors__icontains=keyword)
+                )
+            elif (parameter == 'language'):
+                book_list = Book.objects.filter(
+                    Q(language__icontains=keyword)
+                )
+            elif (parameter == 'publishedDate'):
+                try:
+                    keyword = keyword.split()
+                    keyword.remove('to')
+                    dateStart = keyword[0]
+                    dateEnd = keyword[1]
+                    book_list = Book.objects.filter(
+                        Q(publishedDate__range=[dateStart, dateEnd])
+                    )
+                except:
+                    error = "There is something wrong with date range you have passed. It has to be in following format: 'yyyy-mm-dd to yyy-mm-dd'. If error still occures contact the administrator."
+                    return render(request, 'book_search.html', { 'error':error })
             
             page = request.GET.get('page', 1)
             paginator = Paginator(book_list, 5)
@@ -80,7 +103,7 @@ def book_search(request):
             except EmptyPage:
                 books = paginator.page(paginator.num_pages)
 
-            return render(request, 'book_search.html', { 'books': books })
+            return render(request, 'book_search.html', { 'books': books }) 
         except Exception as exception:
             error = "Something went wrong. If error occurs often please send error message contained below to administator."
             error_message = str(exception)
