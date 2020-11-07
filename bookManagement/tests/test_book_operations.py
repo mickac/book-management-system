42233f24e14f0f2ab504d3f6a77562f8a8fb4f7e
mickac <kaczynski.mp@gmail.com>
2020-11-08 00:18:08 +0100

    Unit tests for simple_search

--- bookManagement/tests/test_book_operations.py
+++ bookManagement/tests/test_book_operations.py
@@ -1,7 +1,11 @@
 import unittest
 
-from ..forms import BookForm
+from django.test import RequestFactory
+from django.db.models import Q
 
+from ..forms import BookForm
+from ..models import Book
+from ..modules.book_operations import BookOperations
 
 class TestBookOperations(unittest.TestCase):
     def test_book_add_or_edit(self):
@@ -27,3 +31,35 @@ class TestBookOperations(unittest.TestCase):
         invalidForm = BookForm(invalidData)
         self.assertTrue(validForm.is_valid())
         self.assertFalse(invalidForm.is_valid())
+
+    def test_simple_search(self):
+        factory = RequestFactory()
+        validKeywordDict = {
+            "keyword": "title",
+            "keyword": "author",
+            "keyword": "language"
+        }
+        validKeywordQ = [
+            Book.objects.filter(Q(title__icontains='title')),
+            Book.objects.filter(Q(authors__icontains='authors')),
+            Book.objects.filter(Q(language__icontains='language'))
+        ]
+        validDateExact = Book.objects.filter(Q(publishedDate__exact="2020-10-31"))
+        validDateRange = Book.objects.filter(Q(publishedDate__range=["2020-10-10","2020-10-30"]))
+        validDateExactRequest = factory.get('/simple_search', {'parameter': 'publishedDate',
+                                                               'dateExact': '2020-10-31'})
+        validDateRangeRequest = factory.get('/simple_search', {'parameter': 'dateRange',
+                                                               'dateFrom': '2020-10-10',
+                                                               'dateTo': "2020-10-30"})       
+        iter = 0
+        for i, j in validKeywordDict.items():
+            iter += iter
+            keyword = {i: j, "parameter": j}
+            validKeywordRequest = factory.get('/simple_search', keyword)
+            testKeyword = BookOperations.simple_search(validKeywordRequest)
+            self.assertEqual(list(testKeyword), list(validKeywordQ[iter]))
+        self.assertEqual(list(validDateExact), 
+                         list(BookOperations.simple_search(validDateExactRequest)))
+        self.assertEqual(list(validDateRange), 
+                         list(BookOperations.simple_search(validDateRangeRequest)))
+
